0eeb694c9cbee0a7c286687f340f8facc4798ab4
Jimmy Zeng <jzengg@gmail.com>
2016-01-28 15:56:02 -0500

    Get permanent access token for fb

--- config/initializers/facebook.rb
+++ config/initializers/facebook.rb
@@ -1,4 +1,1 @@
-user_graph = Koala::Facebook::API.new(ENV['FB_ACCESS_TOKEN'])
-pages = user_graph.get_connections('me', 'accounts')
-page_token = pages.first['access_token']
-$page_graph = Koala::Facebook::API.new(page_token)
+$page_graph = Koala::Facebook::API.new(ENV['FB_ACCESS_TOKEN'])
--- lib/tasks/syndicate.rake
+++ lib/tasks/syndicate.rake
@@ -1,4 +1,4 @@
-namespace :syndicate do
+namespace :syndicater do
   desc "This fetches syndicates new products in the database"
   task publish: :environment do
     ApplicationController.publish
